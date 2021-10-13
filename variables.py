87bcbb2a1fece8c735a3a66c77fb732302d37995
Rajat Rathi <ehrajatrathi@gmail.com>
2021-10-13 12:54:36 +0530

    initial commit

---
+++ variables.py
@@ -0,0 +1,14 @@
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+total=num1+num2+num3+num4
+print(total)
+
+jeans=20
+tshirts=25
+shirts=100
+jackets=200
+total=jeans+tshirts+shirts+jackets
+print("Total Qty:",total)
+
